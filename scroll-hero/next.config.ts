6759e0b82f86938b9911a0dd68d5ddb50a01b1d7
Vish-Bh <173876096+Vish-Bh@users.noreply.github.com>
2026-03-13 19:35:55 +0530

    Configure for static export

--- scroll-hero/next.config.ts
+++ scroll-hero/next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // This creates the "out" folder for GitHub
+  images: {
+    unoptimized: true, // Necessary for static export
+  },
 };
 
 export default nextConfig;
